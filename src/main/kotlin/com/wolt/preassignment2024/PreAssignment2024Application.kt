d757b73b89af563f9a0ceaf6930065cbaf16441b
Timo <timo@seehowyoueat.fi>
2024-01-21 22:37:37 +0200

    chore: init kotlin spring boot project

---
+++ src/main/kotlin/com/wolt/preassignment2024/PreAssignment2024Application.kt
@@ -0,0 +1,11 @@
+package com.wolt.preassignment2024
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PreAssignment2024Application
+
+fun main(args: Array<String>) {
+    runApplication<PreAssignment2024Application>(*args)
+}
---
+++ src/test/kotlin/com/wolt/preassignment2024/PreAssignment2024ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wolt.preassignment2024
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PreAssignment2024ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
