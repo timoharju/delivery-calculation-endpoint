a76a9218db4d49d2f5da19f03e45f29e6f208278
Timo <timo@seehowyoueat.fi>
2024-01-30 20:02:54 +0200

    feat: add constant values

---
+++ src/main/kotlin/com/wolt/preassignment2024/delivery/utils/DeliveryConstants.kt
@@ -0,0 +1,22 @@
+package com.wolt.preassignment2024.delivery.utils
+
+// Delivery fee values are represented in cents
+const val DEFAULT_DELIVERY_FEE: Int = 200
+const val EXTRA_DELIVERY_FEE: Int = 100
+
+const val MAX_DELIVERY_FEE: Int = 1500
+const val HIGH_VALUE_ORDER: Int = 20000
+
+const val MIN_CART_VALUE: Int = 1000
+
+const val LOWER_BULK_ORDER_FEE: Int = 50
+const val HIGHER_BULK_ORDER_FEE: Int = 120
+
+const val RUSH_HOUR_RATE: Double = 1.2
+
+// Distance values are represented in meters
+const val DEFAULT_DISTANCE_THRESHOLD: Int = 1000
+const val ADDITIONAL_DISTANCE_THRESHOLD: Int = 500
+
+const val LOWER_BULK_ORDER_THRESHOLD: Int = 5
+const val HIGHER_BULK_ORDER_THRESHOLD: Int = 12
